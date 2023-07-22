873e494969066240258715ef1739e2d59f8b1506
crossfire3001 <obiwan2035@mail.ru>
2023-07-22 16:26:00 +0500

    bloginfo: name and description in wordpress

--- index.php
+++ index.php
@@ -1,1 +1,11 @@
-This is our amazing custom theme 11111.
+<?php
+ function pasholnahui($name, $color) {
+    echo "<p>Whops who the $name this is you little $color</p>";
+ }
+
+ pasholnahui('Tom', 'grey');
+ pasholnahui('Jerry', 'brown');
+?>
+
+<h1><?php bloginfo('name'); ?></h1>
+<p><?php bloginfo('description'); ?></p>
